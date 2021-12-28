4760d29ca92ebc70f911dc1bd1e23f36461ed90e
Rajat Patil <rajat.r.patil@seagate.com>
2021-12-28 03:55:23 -0700

    EOS-24233 addressed review comment * added function to check if di validation is allowed
    Signed-off-by: Rajat Patil <rajat.r.patil@seagate.com>

--- motr/io_req_fop.c
+++ motr/io_req_fop.c
@@ -303,10 +303,12 @@ static void io_bottom_half(struct m0_sm_group *grp, struct m0_sm_ast *ast)
 	gen_rep = m0_fop_data(m0_rpc_item_to_fop(reply_item));
 	rw_reply = io_rw_rep_get(reply_fop);
 
-	/* Copy attributes to client if reply received from read operation */
+	/*
+	 * Copy attributes to client if reply received from read operation
+	 * Skipping attribute_copy() if cksum validation is not allowed.
+	 */
 	if (m0_is_read_rep(reply_fop) && op->op_code == M0_OC_READ &&
-		ioo->ioo_attr.ov_vec.v_nr && !ioo->ioo_dgmode_io_sent &&
-		!instance->m0c_config->mc_is_read_verify) {
+	    m0__obj_is_cksum_validation_allowed(ioo)) {
 		m0_indexvec_wire2mem(&rwfop->crw_ivec,
 					rwfop->crw_ivec.ci_nr, 0,
 					&rep_attr_ivec);
